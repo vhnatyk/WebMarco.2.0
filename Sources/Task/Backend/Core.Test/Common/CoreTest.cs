bd6283f3c8639e5df8d8bdfd0dd86714245f5bd8
Vitalik Hnatyk <vhnatyk@gmail.com>
2014-10-12 04:40:46 +0300

    Mac port version of example BridgeTry task fixed .

--- Sources/Task/Backend/Core.Test/Common/CoreTest.cs
+++ Sources/Task/Backend/Core.Test/Common/CoreTest.cs
@@ -1,27 +1,45 @@
 ﻿using System;
 using NUnit.Framework;
 using WebMarco.Backend.App.Common;
+using TinyIoC;
+#if MACOSX
+using WebMarco.Backend.App.PlatformImplemented.Mac;
+#elif WIN
+using WebMarco.Backend.App.PlatformImplemented.Win;
+#elif iOS
+using WebMarco.Backend.App.PlatformImplemented.iOS;
+#elif ANDROID
+using WebMarco.Backend.App.PlatformImplemented.Android;
+#else
 
+#endif
 
 namespace BridgeTry.Backend.Core.Test {
     [TestFixture]
     public partial class CoreTest {
 #if MACOSX
 		/// <summary>
-		/// Stub in order to execute MonoMac.AppKit.NSApplication.Init ();//!!!Must be very first thing to run!!!
+		/// Stub in order to execute MonoMac.AppKit.NSApplication.Init ();
 		/// </summary>
 		/// <param name="args">The command-line arguments.</param>
 		static void Main (string[] args)
 		{
 
 		}
+
+		private static bool isNSApplicationInitRunOnce = false;
 #endif
         [SetUp]
         public void Setup() {
 #if MACOSX
-			//essential for stuff like NSHomeDirectory etc
-			MonoMac.AppKit.NSApplication.Init ();//!!!Must be very first thing to run!!!
+			if(!isNSApplicationInitRunOnce)	{
+				//essential for stuff like NSHomeDirectory etc
+				MonoMac.AppKit.NSApplication.Init ();//!!!Must be very first thing to run!!!
+				isNSApplicationInitRunOnce = true;
+			}
 #endif
+			TinyIoCContainer.Current.Register<AppHelper.Data.Manager, Manager>(new Manager());
+			AppHelper.Data.Manager.Instance.RestoreWorkingCopyOfMainDatabase ();//reset main database 
             AppHelper.Data.ConnectDatabase();
         }
 
