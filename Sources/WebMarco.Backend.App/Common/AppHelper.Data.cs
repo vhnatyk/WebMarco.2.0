1303e993b03ae1f7d9f07bee5dfeb6a85a0d9733
Vitaliy Hnatyk <vhnatyk@gmail.com>
2014-10-29 13:50:00 +0200

    Indentation fixed.

--- Sources/WebMarco.Backend.App/Common/AppHelper.Data.cs
+++ Sources/WebMarco.Backend.App/Common/AppHelper.Data.cs
@@ -16,35 +16,47 @@ using Vici.CoolStorage.Xamarin.iOS.Sqlite;
 #endif
 
 
-namespace WebMarco.Backend.App.Common {
-    public static partial class AppHelper {
+namespace WebMarco.Backend.App.Common
+{
+    public static partial class AppHelper
+    {
         #region Data
-        public static class Data {
+        public static class Data
+        {
 
             private const string MainDatabaseName = "master.db"; //TODO: whoa:(  - from config!!! 
-            public static string MainDatabaseFileInAssets {
-                get {
+            public static string MainDatabaseFileInAssets
+            {
+                get
+                {
                     return PathUtils.PathCombineCrossPlatform(AppHelper.Paths.DatafilesAssetPath, MainDatabaseName);
                 }
             }
 
-            public static string MainDatabaseFileInWorkingPath {
-                get {
+            public static string MainDatabaseFileInWorkingPath
+            {
+                get
+                {
                     return PathUtils.PathCombineCrossPlatform(AppHelper.Paths.DatafilesWorkingPath, MainDatabaseName);
                 }
             }
 
-            public static bool ExistsMainDb {
+            public static bool ExistsMainDb
+            {
                 get { return File.Exists(MainDatabaseFileInWorkingPath); }
             }
 
-            public static void ConnectDatabase() {
+            public static void ConnectDatabase()
+            {
                 ConnectDatabase(MainDatabaseFileInWorkingPath);
             }
 
-            public static void ConnectDatabase(string databaseWorkingCopyPath) {
-				try {
-                    if (!FileExists(databaseWorkingCopyPath)) {
+            public static void ConnectDatabase(string databaseWorkingCopyPath)
+            {
+                try
+                {
+                    if (!FileExists(databaseWorkingCopyPath))
+                    {
                         Manager.Instance.RestoreWorkingCopyOfDatabase(databaseWorkingCopyPath);
                     }
 
@@ -53,36 +65,47 @@ namespace WebMarco.Backend.App.Common {
                     DLogger.WriteLog(string.Format("Connected to Db: " + databaseWorkingCopyPath));
 
                     return;
-                } catch (Exception ex) {
+                }
+                catch (Exception ex)
+                {
                     DLogger.WriteLog(ex);
-					throw new Exception(string.Format("Can't connect to database at \"{0}\"", databaseWorkingCopyPath), ex);
+                    throw new Exception(string.Format("Can't connect to database at \"{0}\"", databaseWorkingCopyPath), ex);
                 }
             }
 
-            private static bool FileExists(string filePath) {
+            private static bool FileExists(string filePath)
+            {
                 return File.Exists(filePath);
             }
 
-            public abstract class Manager {
-                public static Manager Instance {
-                    get {
+            public abstract class Manager
+            {
+                public static Manager Instance
+                {
+                    get
+                    {
                         return TinyIoC.TinyIoCContainer.Current.Resolve<Manager>();
                     }
                 }
 
-		public virtual void RestoreWorkingCopyOfDatabase(string databaseWorkingPath) {
-			string dataBaseName = Path.GetFileName(databaseWorkingPath);
-			string databaseInAssetsPath = PathUtils.PathCombineCrossPlatform(AppHelper.Paths.DatafilesAssetPath, dataBaseName);
-			try {
-				File.Delete(databaseWorkingPath);
-			} catch (Exception ex) {
-				DLogger.WriteLog (ex);
-			}
-			File.Copy(databaseInAssetsPath, databaseWorkingPath);
-		}
+                public virtual void RestoreWorkingCopyOfDatabase(string databaseWorkingPath)
+                {
+                    string dataBaseName = Path.GetFileName(databaseWorkingPath);
+                    string databaseInAssetsPath = PathUtils.PathCombineCrossPlatform(AppHelper.Paths.DatafilesAssetPath, dataBaseName);
+                    try
+                    {
+                        File.Delete(databaseWorkingPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        DLogger.WriteLog(ex);
+                    }
+                    File.Copy(databaseInAssetsPath, databaseWorkingPath);
+                }
 
-                public virtual void RestoreWorkingCopyOfMainDatabase() {
-					RestoreWorkingCopyOfDatabase(MainDatabaseFileInWorkingPath);
+                public virtual void RestoreWorkingCopyOfMainDatabase()
+                {
+                    RestoreWorkingCopyOfDatabase(MainDatabaseFileInWorkingPath);
                 }
             }
         }
